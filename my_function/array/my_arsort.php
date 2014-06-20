3c3e69b333aa68d5fb7c6c955e54b4f432577147
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-06-20 18:47:33 +0300

    my function

--- my_function/array/my_arsort.php
+++ my_function/array/my_arsort.php
@@ -3,25 +3,46 @@
 /**
  * 
  */
-function my_arsort($mass_)
-{
-    $size=count($mass_);
-    $array_=array();
+function my_arsort($mass, $flag='')
+{   
+    $size=count($mass);
+    $sortArray=array();
+    $returnArray=array();
+    
+    foreach ($mass as $value){
+        $sortArray[]=$value;
+    }
+    
     for ($i=0; $i < $size; $i++)
     {
         for ($y=($i+1); $y < $size; $y++)
         {
-            if ($mass_[$i] > $mass_[$y])
+            if ($sortArray[$i] < $sortArray[$y])
             {
-                $c = $mass_[$i];
-                $mass_[$i] = $mass_[$y];
-                $mass_[$y] = $c;
+                $c = $sortArray[$i];
+                $sortArray[$i] = $sortArray[$y];
+                $sortArray[$y] = $c;
             }
         }
     }
-    return $mass_;
+    foreach ($sortArray as $key1=>$value1){
+        foreach ($mass as $key2=>$value2){
+            if($value1==$value2){
+                $returnArray[$key2]=$value1;
+            }
+        }
+    }
+    
+    return $returnArray;
 }
 
-$mass=array(8, 5, 2, 3, 6, 9, 7, 4, 1, 0);
-var_dump($mass);
+
+
+$mass=array(11=>8, 22=>5, 33=>2, 44=>3, 55=>6, 66=>9, 77=>7, 88=>4, 99=>1, 100=>0);
 var_dump(my_arsort($mass));
+
+$mass1=array(8, "fgdsgf", 2, 3, "dfgd", 6);
+var_dump(arsort($mass1));
+foreach ($mass1 as $value){
+    echo "$value"."\n";
+}
